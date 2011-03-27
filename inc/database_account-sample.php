e331f6a538795cbf36d007f75aa52146f08d68d9
VesQ <laakso.vesa@gmail.com>
2011-03-27 18:24:02 +0300

    Ainiin, se tiedoston uudelleennimeäminen esimerkiksi.

--- inc/database_account-sample.php
+++ inc/database_account-sample.php
@@ -1,4 +1,5 @@
 <?php
+    // Muuta tiedoston nimi database_account-sample.php -> database_account.php
 	// Tähän tiedostoon omat oikeat tunnukset. Tätä EI laiteta oikeilla tunnuksilla git-repoon!
 	$host = "hostname-or-ip";
 	$db = "databasename";
