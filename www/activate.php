2fc2f56b7ece9787a26f21a7d16e0e5e17ada472
VesQ <laakso.vesa@gmail.com>
2011-04-04 02:54:22 +0300

    Aktivointiin oli unohtunut vanha taulujen prefixin määrittely.

--- www/activate.php
+++ www/activate.php
@@ -1,15 +1,15 @@
 <?php
-    include "inc/database.php";
+    require "inc/database.php";
     
     $h = htmlspecialchars($_GET['hash']);
     
-    if ($stmt = $conn->prepare("UPDATE " . TABLE_PREFIX . "users SET active = 1 WHERE hash = ?")) {
+    if ($stmt = $conn->prepare("UPDATE " . $config['db_prefix'] . "users SET active = 1 WHERE hash = ?")) {
         $stmt->bind_param("s",$h);
         $stmt->execute();
         
         $_SESSION['back'] = "./";
         echo "<meta HTTP-EQUIV='REFRESH' content='0; url=../index.php?pId=valmis&t=2' />";
     } else {
-        echo $conn->error();
+        echo $conn->error;
     }
 ?>
